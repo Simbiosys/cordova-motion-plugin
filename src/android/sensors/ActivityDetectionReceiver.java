3d7db5b0d1c547ad19711418c9ecf040657c3dca
Javier Pérez Arias <javierperezarias@iMac-de-Imac.local>
2019-08-22 16:45:49 +0200

    fix(previous commit): Wrong file was changed in the previous commit.

--- src/android/sensors/ActivityDetectionReceiver.java
+++ src/android/sensors/ActivityDetectionReceiver.java
@@ -3,6 +3,7 @@ package es.simbiosys.cordova.plugin.motion.sensors;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.SystemClock;
 import android.util.Log;
 
 import com.google.android.gms.location.ActivityTransition;
@@ -12,8 +13,12 @@ import com.google.android.gms.location.DetectedActivity;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ActivityDetectionReceiver extends BroadcastReceiver {
 
@@ -29,16 +34,22 @@ public class ActivityDetectionReceiver extends BroadcastReceiver {
           if (ActivityTransitionResult.hasResult(intent)) {
               ActivityTransitionResult result = ActivityTransitionResult.extractResult(intent);
               for (ActivityTransitionEvent event : result.getTransitionEvents()) {
-                  Log.d(TAG, "activity type: " + event.getActivityType());
-                  Log.d(TAG, "transition type: " + event.getTransitionType());
-
+                  Date currentDate = new Date();
                   JSONObject message = new JSONObject();
                   JSONObject eventData = new JSONObject();
 
+                  /* Log.d(TAG, "activity type: " + event.getActivityType());
+                  Log.d(TAG, "transition type: " + event.getTransitionType());
+                  Log.d(TAG, "elapsed real time: " + convertNsToMs(event.getElapsedRealTimeNanos()));
+                  Log.d(TAG, "current time: " + currentDate.getTime()); */
+
+                  long timestamp = currentDate.getTime() - (SystemClock.elapsedRealtime() - convertNsToMs(event.getElapsedRealTimeNanos()));
+
                   try {
                       // Build event data
-                      eventData.put("activityType", getActivityTypeLiteral(event.getActivityType()));
+                      eventData.put("detectedActivities", getDetectedActivities(event.getActivityType()));
                       eventData.put("transitionType", getTransitionTypeLiteral(event.getTransitionType()));
+                      eventData.put("timestamp", getDateString(new Date(timestamp)));
 
                       // Build message
                       message.put("eventName", eventName);
@@ -52,8 +63,8 @@ public class ActivityDetectionReceiver extends BroadcastReceiver {
           }
     }
 
-    private String getTransitionTypeLiteral (int activityType) {
-        switch (activityType) {
+    private String getTransitionTypeLiteral (int transitionType) {
+        switch (transitionType) {
             case ActivityTransition.ACTIVITY_TRANSITION_ENTER:
                 return "ACTIVITY_TRANSITION_ENTER";
             case ActivityTransition.ACTIVITY_TRANSITION_EXIT:
@@ -64,21 +75,40 @@ public class ActivityDetectionReceiver extends BroadcastReceiver {
         }
     }
 
-    private String getActivityTypeLiteral (int transitionType) {
-        switch (transitionType) {
+    private JSONArray getDetectedActivities (int activityType) {
+        JSONArray detectedActivities = new JSONArray();
+
+        switch (activityType) {
             case DetectedActivity.IN_VEHICLE:
-                return "IN_VEHICLE";
+                detectedActivities.put("IN_VEHICLE");
+                break;
             case DetectedActivity.ON_BICYCLE:
-                return "ON_BICYCLE";
+                detectedActivities.put("ON_BICYCLE");
+                break;
             case DetectedActivity.RUNNING:
-                return "RUNNING";
+                detectedActivities.put("RUNNING");
+                break;
             case DetectedActivity.STILL:
-                return "STILL";
+                detectedActivities.put("STILL");
+                break;
             case DetectedActivity.WALKING:
-                return "WALKING";
+                detectedActivities.put("WALKING");
+                break;
             default:
-                return "UNKNOWN";
+                detectedActivities.put("UNKNOWN");
         }
+
+        return detectedActivities;
+    }
+
+    private String getDateString(Date date) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        return formatter.format(date);
+    }
+
+    private long convertNsToMs(long ns) {
+        return ns/1000000;
     }
 
     public void triggerJsEvent(JSONObject message) {
